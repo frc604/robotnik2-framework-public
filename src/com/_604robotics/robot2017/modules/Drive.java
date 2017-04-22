55b52785e3b8e8220ec3fabe1e9e267a74cce15b
rlee287 <rlee287@yahoo.com>
2017-04-22 08:27:08 -0700

    Move encoder ports to Ports.java and add Arcade Drive Action

--- src/com/_604robotics/robot2017/modules/Drive.java
+++ src/com/_604robotics/robot2017/modules/Drive.java
@@ -1,5 +1,6 @@
 package com._604robotics.robot2017.modules;
 
+import com._604robotics.robot2017.constants.Ports;
 import com._604robotics.robotnik.Action;
 import com._604robotics.robotnik.Input;
 import com._604robotics.robotnik.Module;
@@ -13,8 +14,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Drive extends Module {
     private final RobotDrive robotDrive = new RobotDrive(1, 2);
 
-    private final Encoder encoderLeft = new Encoder(1, 2, false, CounterBase.EncodingType.k4X);
-    private final Encoder encoderRight = new Encoder(3, 4, true, CounterBase.EncodingType.k4X);
+    private final Encoder encoderLeft = new Encoder(Ports.ENCODER_LEFT_A,
+    												Ports.ENCODER_LEFT_B, 
+    												false,
+    												CounterBase.EncodingType.k4X);
+    private final Encoder encoderRight = new Encoder(Ports.ENCODER_RIGHT_A,
+    												Ports.ENCODER_RIGHT_B,
+    												true,
+    												CounterBase.EncodingType.k4X);
     
     public final Output<Integer> leftClicks = addOutput("leftClicks", encoderLeft::get);
     public final Output<Integer> rightClicks = addOutput("rightClicks", encoderRight::get);
@@ -49,6 +56,26 @@ public class Drive extends Module {
         @Override
         public void run () {
             robotDrive.tankDrive(leftPower.get(), rightPower.get());
+        }
+    }
+    
+    public class ArcadeDrive extends Action {
+        public final Input<Double> movePower = addInput("movePower", 0d);
+        public final Input<Double> rotatePower = addInput("rotatePower", 0d);
+
+        public ArcadeDrive () {
+            super(Drive.this, ArcadeDrive.class);
+        }
+
+        public ArcadeDrive (double initialLeftPower, double initialRightPower) {
+            this();
+            movePower.set(initialLeftPower);
+            rotatePower.set(initialRightPower);
+        }
+
+        @Override
+        public void run () {
+            robotDrive.arcadeDrive(movePower.get(), rotatePower.get());
         }
     }
 
