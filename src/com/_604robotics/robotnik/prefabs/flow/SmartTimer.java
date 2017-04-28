a1bfa800bb0119d45eedecf07c7962dab4e3d4fd
Michael Smith <michael@spinda.net>
2017-04-27 18:08:06 -0700

    Make SmartTimer take Supplier as well as Runnable

--- src/com/_604robotics/robotnik/prefabs/flow/SmartTimer.java
+++ src/com/_604robotics/robotnik/prefabs/flow/SmartTimer.java
@@ -2,6 +2,8 @@ package com._604robotics.robotnik.prefabs.flow;
 
 import edu.wpi.first.wpilibj.Timer;
 
+import java.util.function.Supplier;
+
 public class SmartTimer extends Timer {
     private boolean running;
 
@@ -30,10 +32,32 @@ public class SmartTimer extends Timer {
         }
     }
 
+    public <T> T runUntil (double time, T defaultValue, Supplier<T> supplier) {
+        if (!hasReachedTime(time)) {
+            return supplier.get();
+        }
+        return defaultValue;
+    }
+
+    public <T> T runUntil (double time, Supplier<T> supplier) {
+        return runUntil(time, null, supplier);
+    }
+
     public void runAfter (double time, Runnable runnable) {
         if (hasReachedTime(time)) {
             runnable.run();
         }
+    }
+
+    public <T> T runAfter (double time, T defaultValue, Supplier<T> supplier) {
+        if (hasReachedTime(time)) {
+            return supplier.get();
+        }
+        return defaultValue;
+    }
+
+    public <T> T runAfter (double time, Supplier<T> supplier) {
+        return runAfter(time, null, supplier);
     }
 
     public void runEvery (double time, Runnable runnable) {
@@ -41,6 +65,19 @@ public class SmartTimer extends Timer {
             runnable.run();
             reset();
         }
+    }
+
+    public <T> T runEvery (double time, T defaultValue, Supplier<T> supplier) {
+        if (hasReachedTime(time)) {
+            final T value = supplier.get();
+            reset();
+            return value;
+        }
+        return defaultValue;
+    }
+
+    public <T> T runEvery (double time, Supplier<T> supplier) {
+        return runEvery(time, null, supplier);
     }
 
     @Override
