5407fa76e5e0ad00f16fae03888a1f32e2f5866b
Michael Smith <michael@spinda.net>
2017-04-28 01:40:39 -0700

    Rework StatefulController in terms of Coordinators
    Instead of having their own State inner class, they
    now use a list of Coordinators, advancing to the next
    Coordinator in the list when each one stops running
    (returning false from its run() hook).

---
+++ src/com/_604robotics/robotnik/prefabs/coordinators/StatefulCoordinator.java
@@ -0,0 +1,86 @@
+package com._604robotics.robotnik.prefabs.coordinators;
+
+import com._604robotics.robotnik.Coordinator;
+import com._604robotics.robotnik.Logger;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public abstract class StatefulCoordinator extends Coordinator {
+    private final Logger logger;
+
+    private List<Pair<String, Coordinator>> states = new ArrayList<>();
+    private int stateIndex;
+
+    public StatefulCoordinator (String name) {
+        logger = new Logger(StatefulCoordinator.class, name);
+    }
+
+    public StatefulCoordinator (Class klass) {
+        this(klass.getSimpleName());
+    }
+
+    public void addState (String name, Coordinator coordinator) {
+        states.add(new Pair<>(name, coordinator));
+    }
+
+    public void addState (Class klass, Coordinator coordinator) {
+        addState(klass.getSimpleName(), coordinator);
+    }
+
+    @Override
+    public void begin () {
+        logger.info("Begin");
+        stateIndex = 0;
+        enterState();
+    }
+
+    @Override
+    public boolean run () {
+        final Pair<String, Coordinator> currentState = getCurrentState();
+        if (currentState != null) {
+            if (!currentState.getValue().execute()) {
+                exitState();
+                ++stateIndex;
+                return enterState();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void end () {
+        exitState();
+        logger.info("End");
+    }
+
+    private boolean enterState () {
+        final Pair<String, Coordinator> currentState = getCurrentState();
+        if (currentState == null) {
+            logger.info("Null State");
+            return false;
+        }
+
+        logger.info("Enter State: " + currentState.getKey());
+        currentState.getValue().start();
+        return true;
+    }
+
+    private void exitState () {
+        final Pair<String, Coordinator> currentState = getCurrentState();
+        if (currentState != null) {
+            currentState.getValue().stop();
+            logger.info("Exit State: " + currentState.getKey());
+        }
+    }
+
+    private Pair<String, Coordinator> getCurrentState () {
+        if (stateIndex >= states.size()) {
+            return null;
+        }
+        return states.get(stateIndex);
+    }
+}
