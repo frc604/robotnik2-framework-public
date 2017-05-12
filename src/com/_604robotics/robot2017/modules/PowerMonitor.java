1e7ad5a2ce267420f2946d12b1e68e0e3e4cd2bc
rlee287 <rlee287@yahoo.com>
2017-05-11 23:00:16 -0700

    Log when power exceeds certain threshold
    The precise values will need to be calibrated after discussions with the electrical team

--- src/com/_604robotics/robot2017/modules/PowerMonitor.java
+++ src/com/_604robotics/robot2017/modules/PowerMonitor.java
@@ -1,21 +1,44 @@
 package com._604robotics.robot2017.modules;
 
 import com._604robotics.robot2017.constants.Ports;
+import com._604robotics.robotnik.Logger;
 import com._604robotics.robotnik.Module;
 import com._604robotics.robotnik.Output;
+import com._604robotics.robotnik.prefabs.flow.SmartTimer;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 public class PowerMonitor extends Module {
 
-    private final PowerDistributionPanel panel = new PowerDistributionPanel(Ports.PDP_MODULE);
-    public Output<Double> totalPower;
-    public Output<Double> batteryVoltage;
-    public Output<Double> [] currents;// = new Output[16];
+    private class emptyOutput implements Output<Double> {
+        @Override
+        public Double get() {
+            return 0D;
+        }
+    }
+
+    private static final PowerDistributionPanel panel = new PowerDistributionPanel(Ports.PDP_MODULE);
+    public static Output<Double> totalCurrent;
+    public static Output<Double> batteryVoltage;
+
+    public static Output<Double> [] currents = new emptyOutput[16];
+    public static double [] currentLimit = {40, 40, 40, 40,
+                                     20, 20, 20, 20,
+                                     20, 20, 20, 20,
+                                     40, 40, 40, 40};
+
+    static {
+        for (int i=0;i<currentLimit.length;i++) {
+            currentLimit[i]*=0.8;
+        }
+    }
+
+    private static final Logger theLogger = new Logger(PowerMonitor.class);
+    private final SmartTimer iterTimer = new SmartTimer();
 
     public PowerMonitor() {
         super(PowerMonitor.class);
-        totalPower=addOutput("Total Power", () -> panel.getTotalCurrent());
+        totalCurrent=addOutput("Total Current", () -> panel.getTotalCurrent());
         batteryVoltage=addOutput("Battery Voltage", () -> panel.getVoltage());
         /*for (int port=0;port<=15;port++) {
             currents[port]=addOutput("Current "+port, () -> panel.getCurrent(port));
@@ -37,7 +60,29 @@ public class PowerMonitor extends Module {
         currents[13]=addOutput("Current 13", () -> panel.getCurrent(13));
         currents[14]=addOutput("Current 14", () -> panel.getCurrent(14));
         currents[15]=addOutput("Current 15", () -> panel.getCurrent(15));
-
+    }
+    
+    private Runnable checkCurrent = new Runnable() {
+        public void run() {
+            for (int i=0;i<currents.length;i++) {
+                if (currents[i].get() > currentLimit[i]) {
+                    theLogger.log("WARN", "Too much current through PDP Port "+i+"!");
+                }
+            }
+        }
+    };
+    
+    protected void begin() {
+        iterTimer.start();
+    }
+    
+    protected void run() {
+        // TODO: Find good time interval
+        iterTimer.runEvery(0.5, checkCurrent);
+    }
+    
+    protected void end() {
+        iterTimer.stopAndReset();
     }
 
 }
