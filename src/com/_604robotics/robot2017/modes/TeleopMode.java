baebca54ca9a1ab5c8c9160024f979503675df46
rlee287 <rlee287@yahoo.com>
2017-05-22 15:45:10 -0700

    Properly retract intake

--- src/com/_604robotics/robot2017/modes/TeleopMode.java
+++ src/com/_604robotics/robot2017/modes/TeleopMode.java
@@ -182,6 +182,8 @@ public class TeleopMode extends Coordinator {
 
             if (extend) {
                 robot.flipFlop.extend.activate();
+            } else {
+                robot.flipFlop.retract.activate();
             }
 
             if (driver.buttons.y.get()) {
@@ -195,7 +197,6 @@ public class TeleopMode extends Coordinator {
             }
 
             switch (intakeState) {
-                // TODO: Check that I bound the right enum state to the right action
                 case FORWARD:
                     robot.intake.spit.activate();
                     break;
