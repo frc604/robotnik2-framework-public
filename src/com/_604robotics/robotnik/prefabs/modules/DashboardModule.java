02a5de3b659304995f3cb32c09859d977e2c1a61
rlee287 <rlee287@yahoo.com>
2017-07-06 18:38:27 -0700

    Put chooser onto NetworkTable

--- src/com/_604robotics/robotnik/prefabs/modules/DashboardModule.java
+++ src/com/_604robotics/robotnik/prefabs/modules/DashboardModule.java
@@ -63,6 +63,7 @@ public class DashboardModule extends Module {
 
     protected <E extends Enum<E>> Output<E> addDashboardOutput (String name, E defaultValue, Class<E> klass) {
         final SendableChooser<E> chooser = new SendableChooser<>();
+        SmartDashboard.putData(name,chooser);
         for (E option : EnumSet.allOf(klass)) {
             if (option == defaultValue) {
                 chooser.addDefault(option.toString(), option);
