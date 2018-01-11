58d075690756a6c341551a3ce8dd8bb1f15727d4
2017Driver <2017-Why-Try-To-Email-Me@spilled.java>
2018-01-11 15:32:17 -0800

    Remove untested banner after testing session
    (cherry picked from commit 974994c2d62ef44f1c7373e31e93f11acb01f02e)

--- src/com/_604robotics/robotnik/prefabs/modules/PowerMonitor.java
+++ src/com/_604robotics/robotnik/prefabs/modules/PowerMonitor.java
@@ -10,12 +10,6 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-//UNTESTED
-//             _            _           _
-// _   _ _ __ | |_ ___  ___| |_ ___  __| |
-//| | | | '_ \| __/ _ \/ __| __/ _ \/ _` |
-//| |_| | | | | ||  __/\__ \ ||  __/ (_| |
-// \__,_|_| |_|\__\___||___/\__\___|\__,_|
 public class PowerMonitor extends Module {
 
     private final PowerDistributionPanel panel;
