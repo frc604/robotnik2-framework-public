d8bfc21075e67a1fde4abb91e7d10a50bc2ac16b
2017Driver <2017-Why-Try-To-Email-Me@spilled.java>
2018-01-11 15:31:33 -0800

    Cleanup powermon stuff in preparation of copying over to 2018
    (cherry picked from commit 3aebe2cc27cafe5e753c8930366e94bc462424a5)

--- src/com/_604robotics/robotnik/prefabs/modules/PowerMonitor.java
+++ src/com/_604robotics/robotnik/prefabs/modules/PowerMonitor.java
@@ -8,6 +8,7 @@ import com._604robotics.robotnik.prefabs.flow.SmartTimer;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 //UNTESTED
 //             _            _           _
@@ -51,17 +52,6 @@ public class PowerMonitor extends Module {
     private Pulse [] pulseArray = new Pulse[NUM_CURRENT_PORTS];
     
     private static final Logger theLogger = new Logger(PowerMonitor.class);
-
-    private Runnable checkCurrent = new Runnable() {
-        public void run() {
-            for (int i=0;i<currents.length;i++) {
-                if (currents[i].get() > currentLimit[i]) {
-                    theLogger.log("WARN", "Excess current of "+currents[i].get()+" through PDP Port "+i+"!");
-                }
-            }
-        }
-    };
-
     private void resetTimers() {
         for (int i=0;i<NUM_CURRENT_PORTS;i++) {
             timerArray[i]=new SmartTimer();
@@ -93,6 +83,10 @@ public class PowerMonitor extends Module {
                 theLogger.log("WARN","null tempOutput");
             }
         }
+    }
+    
+    public void initDashboardSendables() {
+        SmartDashboard.putData("PDP", panel);
     }
     protected void begin() {
         resetTimers();
@@ -149,98 +143,5 @@ public class PowerMonitor extends Module {
 
     protected void end() {
         resetTimers();
-    }
-
-    private class MonitorThread extends Thread {
-        private boolean keepRunning=true;
-        private SmartTimer [] timerArray = new SmartTimer[NUM_CURRENT_PORTS];
-        private SmartTimer voltageTimer = new SmartTimer();
-        private Pulse voltagePulse = new Pulse();
-        private Pulse [] pulseArray = new Pulse[NUM_CURRENT_PORTS];
-
-        public void stopRun() {
-            // Ignoring potential race condition here
-            // Does not matter if run loop runs one more time than it should
-            keepRunning=false;
-        }
-        @Override
-        public void run() {
-            theLogger.info("Starting Power Monitoring Thread");
-            for (int i=0;i<NUM_CURRENT_PORTS;i++) {
-                timerArray[i]=new SmartTimer();
-                timerArray[i].stopAndReset();
-                pulseArray[i]=new Pulse();
-                pulseArray[i].update(false);
-            }
-            voltageTimer.stopAndReset();
-            voltagePulse.update(false);
-
-            for (int port=0;port<NUM_CURRENT_PORTS;port++) {
-                int proxy=port;
-                Output<Double> tempOutput = addOutput("Port "+port+" Current", () -> panel.getCurrent(proxy));
-                currents[proxy]=tempOutput;
-            }
-            double[] currentLimit = {40, 40, 40, 40,
-                    20, 20, 20, 20,
-                    20, 20, 20, 20,
-                    40, 40, 40, 40};
-
-            for (int i=0;i<NUM_CURRENT_PORTS;i++) {
-                currentLimit[i]*=0.8;
-            }
-
-            // Main monitor loop
-            while (keepRunning && !isInterrupted()) {
-                for (int i=0;i<NUM_CURRENT_PORTS;i++) {
-                    if (timerArray[i].isRunning()) {
-                        if ((timerArray[i].get()%0.5)<0.001 && (timerArray[i].get()>0.4)) {
-                            theLogger.log("WARN", "Excess current of "+currents[i].get()+" through PDP Port "+i+"!");
-                            theLogger.log("WARN", "Excess has continued for "+timerArray[i].get()+" seconds.");
-                        }
-                    }
-                }
-                if (voltageTimer.isRunning()) {
-                    if ((voltageTimer.get()%0.5)<0.001 && (voltageTimer.get()>0.4)) {
-                        theLogger.log("WARN", "Low voltage of "+batteryVoltage.get()+"V!");
-                        theLogger.log("WARN", "Voltage drop has continued for "+voltageTimer.get()+" seconds.");
-                    }
-                }
-                Thread.yield();
-                for (int i=0;i<NUM_CURRENT_PORTS;i++) {
-                    if (currents[i].get() > currentLimit[i]) {
-                        pulseArray[i].update(true);
-                        timerArray[i].start();
-                        if (pulseArray[i].isRisingEdge()) {
-                            theLogger.log("WARN", "Excess current started through PDP Port "+i+"!");
-                        }
-                    } else {
-                        pulseArray[i].update(false);
-                        timerArray[i].stop();
-                        if (pulseArray[i].isFallingEdge()) {
-                            theLogger.log("WARN", "Excess current ended through PDP Port "+i+" after "+timerArray[i].get()+" seconds.");
-                        }
-                        timerArray[i].reset();
-                    }
-                }
-                if (batteryVoltage.get()<voltageLimit) {
-                    voltagePulse.update(true);
-                    voltageTimer.start();
-                    if (voltagePulse.isRisingEdge()) {
-                        theLogger.log("WARN", "Voltage has dropped below "+voltageLimit+"V!");
-                    }
-                } else {
-                    voltagePulse.update(false);
-                    voltageTimer.stop();
-                    if (voltagePulse.isFallingEdge()) {
-                        theLogger.log("WARN", "Voltage has risen above "+voltageLimit+"V!");
-                    }
-                }
-                Thread.yield();
-            }
-            for (SmartTimer timer : timerArray) {
-                timer.stopAndReset();
-            }
-            theLogger.info("Stopping Power Monitoring Thread");
-        }
     }
 }
